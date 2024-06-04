5a7ecd98ccb670746927c68563fdd1f0a380d823
ninager <gninapaucar@frba.utn.edu.ar>
2024-06-04 19:37:41 -0300

    esto es una prueba

---
+++ prueba.hs
@@ -0,0 +1,10 @@
+import Text.Show.Functions()
+
+data Ciudad = Ciudad {
+    nombre :: String,
+    anioDeFundacion :: Int,
+    atracciones :: [String]
+} deriving(Show, Eq)
+
+haskellandia :: Ciudad
+haskellandia = Ciudad "Haskellandia" 1930 ["Parque Haskell", "Campo Feliz"]
